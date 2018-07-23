d3ba0bc35898de7f717214407f73bee55c1ae80e
Dmitriy Kireev <chief_programmer@mail.ru>
2018-07-23 15:14:18 +0300

    project start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="App" />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
