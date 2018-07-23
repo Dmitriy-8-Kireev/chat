aad8bc054fe971f3cb8099b1b32c7dd74852c3be
Dmitriy Kireev <chief_programmer@mail.ru>
2018-07-23 15:22:13 +0300

    add Title component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component, Fragment } from 'react';
+import Title from './components/Title';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <div className="App" />
+        <div className="App">
+          <Title />
+        </div>
       </Fragment>
     );
   }
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import '../index.css';
+
+class Title extends Component {
+  render() {
+    return <p className="title">My awesome chat</p>;
+  }
+}
+export default Title;
